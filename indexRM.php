f47f155f2265a864ad791bd8bb52dfad7dc4523a
RobinsonRobert <robert.mielcarz@wp.pl>
2018-03-19 00:16:03 +0100

    Zmiany

--- indexRM.php
+++ indexRM.php
@@ -1,64 +1,82 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Wekert</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="mainRM.css">
+</head>
+<body>
+    <header>
+        <!-- Inwestycja -->
+        <section clas="top">
+            <div class="NazwaInwestycji">
+                <form action="indexRM.html">
+                    <label class="opis"for="WprInwestycji">Nazwa inwestycji</label><br>
+                    <input type="text" name="WprInwestycji">
+                    <input type="checkbox" name="komplikacja" value="Skomplikowany">
+                    <label for="komplikacja">Utrudnienie</label>
+                </form>
+            </div>
+        </section>
+    </header>
+        <hr>
+    <dane>
+        <!-- Główne parametry inwestycji -->
+        <section class="blok1">
+            <div class="PowierzchniaGrubosc">
+                <form action="" method="post">
+                    <label for="Powierzchnia" class="etykieta1">Pomiar powierzchni</label><br>
+                    <input type="text" name = "Powierzchnia1"><label> m2</label><br>
+                    <label for="Grubosc">Grubość izolacji</label><br>
+                    <input type="text" name ="Grubosc"><label for="Grubosc"> cm</label><br>
+                    
+                    <?php
+                      if(isset($_POST['submit'])){ ?>
+                        <hr>
+                        <label>Pomiar drugiej powierzchni</label>
+                        <input type="int" name='Powierzchnia2'><label> m2</label><br>
+                        <label>Grubość drugiej izolacji</label>
+                        <input type="int" name='Grubość2'><label> cm</label><br>
+                        <button type="submit" name="submit2" value="Usuń">Usuń</button>
+                    <?php } ?>
 
-$komplikacja = 1;
-$iloscM2 = 451;
 
-if ($komplikacja == 1) {
-    echo komplikacja ($iloscM2);
-} else {
-    echo standard ($iloscM2);
-}
-
-
-function standard ($powierzchnia) {
-   $standard = [220, 400, 580, 760, 910];                       //Standardowe powierzchnie
+                    <button type="submit" name="submit" value="Dodaj">Dodaj</button>
+                </form>
+            </div>
+        </section>
+        <!-- Dane materiałowe -->
+        <section class="blok1">
+            <div class="MaterialZakupKurs">
+                <form action="indexRM.html">
+                    <label for="material">Materiał</label><br>
+                    <select name="material" id="a1">
+                        <option>Crossin</option>
+                        <option>Ekoprodur S0310</option>
+                        <option>AlmaColor</option>
+                    </select><br>
+                    <label for="KosztZakupu">Koszt zakupu</label><br>
+                    <input type="text" name = "KosztZakupu"> 
+                        <select name="waluta" id="a2">
+                            <option>euro/kg</loption>
+                            <option>zł/kg</option>
+                        </select><br>
+                    <label for="KursRuro">Kurs euro</label><br>
+                    <input type="text" name = "KursEuro"> <label> zł/euro</label> 
+                </form>
+            </div>
+        </section>
+        
+    <div class="PrzyciskWyslij"> 
+        <input class="przycisk" type="submit" name ="wyslij" value="Wyślij">
+    </div> 
+    </dane>
    
+    <footer>
+        <p>&copy; Opracowano - WEKERT</p>
+    </footer>
 
-    if($powierzchnia <= $standard[0]) {
-       echo $iloscDni = 1 . ' dzień - obiekt standardowy';
-    }
-    elseif ($powierzchnia >= $standard[0] && $powierzchnia <= $standard[1]){
-        echo $iloscDni = 2 .' dni - obiekt standardowy';
-        }
-    elseif ($powierzchnia >= $standard[1] && $powierzchnia <= $standard[2]){
-        echo $iloscDni = 3 . ' dni - obiekt standardowy';
-    }
-    elseif ($powierzchnia >= $standard[2] && $powierzchnia <= $standard[3]){
-        echo $iloscDni = 4 . ' dni - obiekt standardowy';
-    }
-    elseif ($powierzchnia >= $standard[3] && $powierzchnia <= $standard[4]) {
-        echo $iloscDni = 5 . ' dni - obiekt standardowy';
-    }
-    else {
-        echo "Realizacja inwestycji powyżej 1 tygodnia";
-    }    
-}
-
-function komplikacja ($powierzchnia) {
-    $komplikacja = [150,300,450,600,750];                       //Skomplikowane powierzchnie
-    
-    if($powierzchnia <= $komplikacja[0]) {
-       echo $iloscDni = 1 . ' dzień - obiekt skopmplikowany';
-    }
-    elseif ($powierzchnia >= $komplikacja[0] && $powierzchnia <= $komplikacja[1]){
-        echo $iloscDni = 2 . ' dni - obiekt skomplikowany';
-        }
-    elseif ($powierzchnia >= $komplikacja[1] && $powierzchnia <= $komplikacja[2]){
-        echo $iloscDni = 3 . ' dni - obiekt skomplikowany';
-    }
-    elseif ($powierzchnia >= $komplikacja[2] && $powierzchnia <= $komplikacja[3]){
-        echo $iloscDni = 4 . ' dni - obiekt skomplikowany';
-    }
-    elseif ($powierzchnia >= $komplikacja[3] && $powierzchnia <= $komplikacja[4]) {
-        echo $iloscDni = 5 . ' 
-        1`dni - obiekt skomplikowany';
-    }
-    else {
-        echo "Realizacja inwestycji powyżej 1 tygodnia";
-    }    
-}
-
-
-
-
-?>
+</body>
+</html>
---
+++ utrudnienie.php
@@ -0,0 +1,64 @@
+<?php
+
+$komplikacja = 1;
+$iloscM2 = 451;
+
+if ($komplikacja == 1) {
+    echo komplikacja ($iloscM2);
+} else {
+    echo standard ($iloscM2);
+}
+
+
+function standard ($powierzchnia) {
+   $standard = [220, 400, 580, 760, 910];                       //Standardowe powierzchnie
+   
+
+    if($powierzchnia <= $standard[0]) {
+       echo $iloscDni = 1 . ' dzień - obiekt standardowy';
+    }
+    elseif ($powierzchnia >= $standard[0] && $powierzchnia <= $standard[1]){
+        echo $iloscDni = 2 .' dni - obiekt standardowy';
+        }
+    elseif ($powierzchnia >= $standard[1] && $powierzchnia <= $standard[2]){
+        echo $iloscDni = 3 . ' dni - obiekt standardowy';
+    }
+    elseif ($powierzchnia >= $standard[2] && $powierzchnia <= $standard[3]){
+        echo $iloscDni = 4 . ' dni - obiekt standardowy';
+    }
+    elseif ($powierzchnia >= $standard[3] && $powierzchnia <= $standard[4]) {
+        echo $iloscDni = 5 . ' dni - obiekt standardowy';
+    }
+    else {
+        echo "Realizacja inwestycji powyżej 1 tygodnia";
+    }    
+}
+
+function komplikacja ($powierzchnia) {
+    $komplikacja = [150,300,450,600,750];                       //Skomplikowane powierzchnie
+    
+    if($powierzchnia <= $komplikacja[0]) {
+       echo $iloscDni = 1 . ' dzień - obiekt skopmplikowany';
+    }
+    elseif ($powierzchnia >= $komplikacja[0] && $powierzchnia <= $komplikacja[1]){
+        echo $iloscDni = 2 . ' dni - obiekt skomplikowany';
+        }
+    elseif ($powierzchnia >= $komplikacja[1] && $powierzchnia <= $komplikacja[2]){
+        echo $iloscDni = 3 . ' dni - obiekt skomplikowany';
+    }
+    elseif ($powierzchnia >= $komplikacja[2] && $powierzchnia <= $komplikacja[3]){
+        echo $iloscDni = 4 . ' dni - obiekt skomplikowany';
+    }
+    elseif ($powierzchnia >= $komplikacja[3] && $powierzchnia <= $komplikacja[4]) {
+        echo $iloscDni = 5 . ' 
+        1`dni - obiekt skomplikowany';
+    }
+    else {
+        echo "Realizacja inwestycji powyżej 1 tygodnia";
+    }    
+}
+
+
+
+
+?>
